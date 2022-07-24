181998293f0981e6f9c6d000b163c10cbf61aa6b
Prerit Dayal <prerit714@gmail.com>
2022-07-25 03:46:38 +0530

    Removed classes and added immutable interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,11 +1,25 @@
-import { Product } from "./classes";
-
 export interface IUser {
   username: string;
-  password: string;
+  password?: string;
   role: "buyer" | "seller";
+  cart?: ReadonlyArray<IProduct>;
+  wishList?: ReadonlyArray<IProduct>;
+  transactions?: ReadonlyArray<ITransaction>;
+}
+
+export interface IProduct {
+  id: string;
+  name: string;
+  price: number;
+  soldTo?: IUser;
+  soldBy?: IUser;
 }
 
 export interface IRepository {
-  products: Array<Product>;
+  products: ReadonlyArray<IProduct>;
+}
+
+export interface ITransaction {
+  type: "+" | "-";
+  amount: number;
 }
